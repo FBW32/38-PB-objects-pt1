3a7e5ad8a79c27d8f83621738c6685e2d9cddea9
arezoofaraji <arezoo.faraji66@gmail.com>
2020-09-17 11:45:50 +0200

    bonus-added

--- arezoo/script.js
+++ arezoo/script.js
@@ -31,6 +31,57 @@ function personPrintInfo(person) {
 }
 console.log(personPrintInfo(person));
 
-//bonus
+//1
 
+let objectToArray = {
+  A: 1,
+  B: 2,
+  C: 3,
+};
 
+let entries = Object.entries(objectToArray);
+console.log(entries);
+
+//2
+let student = {
+  name: "Mike",
+  class: "4A",
+  course: "English",
+};
+let keys = (arr) => {
+  console.log(Object.keys(arr));
+};
+keys(student);
+
+//3
+let first = { firstName: "John" };
+let last = { lastName: "Smith" };
+let newObj = (arr1, arr2) => {
+  let result = { ...arr1, ...arr2 };
+  return result;
+};
+console.log(newObj(first, last));
+
+//4
+let person3 = {
+  name: "John",
+  job: "teacher",
+};
+
+let res = Object.assign(
+  {},
+  ...Object.entries(person3).map(([prevKey, val]) => ({
+    [person3[prevKey]]: prevKey,
+  }))
+);
+console.log(res);
+//5
+let firstArray = { a: 1, b: 2, c: 3 };
+
+let result = (arr) => {
+  const values = Object.values(arr);
+  const keys = Object.keys(arr);
+  const result = [keys, values];
+  return result;
+};
+console.log(result(firstArray));
